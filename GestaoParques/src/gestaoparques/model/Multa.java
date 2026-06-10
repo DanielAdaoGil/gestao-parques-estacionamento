a5d3b2d84a0a820dc29d245d0d12908b773d4cc0
António Manuel <antonioqueta74@gmail.com>
2026-06-10 20:09:55 +0200

    Trabalhei nas classes viaturas, multas e parque

---
+++ GestaoParques/src/gestaoparques/model/Multa.java
@@ -0,0 +1,53 @@
+package gestaoparques.model;
+public class Multa{
+    private String matricula;
+    private double valor;
+    private String tipo;
+    private int hora;
+    
+    
+    public Multa(String matricula, double valor, String tipo, int hora) {
+        this.matricula = matricula;
+        this.valor = valor;
+        this.tipo = tipo;
+        this.hora = hora;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public int getHora() {
+        return hora;
+    }
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+    @Override
+    public String toString() {
+        return getMatricula() + ";" + getValor() + ";" + getTipo()
+                + ";" + getHora();
+    }
+    public static Multa fromMulta(String linha){
+        String[] parte = linha.split(";");
+        Multa multa = new Multa(parte[0], Double.parseDouble(parte[1]), parte[2], Integer.parseInt(parte[3]));
+            return multa;
+    }
+    public String mostrarMulta(){
+        return "\nMultas\nMatricula: " +getMatricula() +"\nValor: " + getValor() +"\nTipo: " 
+                + getTipo()+"\nHora: " + getHora();
+    }
+}
---
+++ GestaoParques/src/gestaoparques/model/texte.java
@@ -0,0 +1,24 @@
+package gestaoparques.model;
+
+public class texte {
+    public static void main(String[] args) {
+        //ÁREA DA VIATURA
+        /*Viatura v = new Viatura(null, 0);
+        v.setMatricula("AMJ-17-05");
+        v.setParque("P001-Unitel");
+        v.setEspaco(1);
+        v.setHoraEntrada(10);
+        v.setMultada(false);
+        System.out.println(v.toString());*/
+        //ÁREA DA MULTA
+        /*Multa m = new Multa("AJ-17-05-LD", 2, "NAO", 12);
+        m.setMatricula("MLA-23-45");
+        m.setValor(17);
+        m.setTipo("MultaInv");
+        m.setHora(2);
+        System.out.println(m.toString());*/
+        /*String str = "Esta string contém cinco palavras";
+        String[] arrayOfWords = str.split(" ");
+        System.out.println(arrayOfWords.length);*/
+    }
+}
